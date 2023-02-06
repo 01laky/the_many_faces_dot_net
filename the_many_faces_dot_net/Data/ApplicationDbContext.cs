dbc51c0a499d4d407a844c8e434326736359c172
Ladislav Kostolny <ladislavkostolny@192.168.0.103>
2023-02-06 15:54:59 +0100

    Just initialized .net and React builds

---
+++ the_many_faces_dot_net/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using the_many_faces_dot_net.Models;
+
+namespace the_many_faces_dot_net.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions)
+    {
+
+    }
+}
+
---
+++ the_many_faces_dot_net/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace the_many_faces_dot_net.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
+
